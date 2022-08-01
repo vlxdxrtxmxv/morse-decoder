909a6d2c6ce6aca211a9910a98640d6a336f4f67
vlxdxrtxmxv <103958051+vlxdxrtxmxv@users.noreply.github.com>
2022-08-01 13:35:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,59 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let transferInNum = [];
+
+    let defaultSymbol = {
+        '00': '',
+        '10': '.',
+        '11': '-',
+    }
+
+
+    for (let i = 0; i < expr.length; i += 10) {
+        let decoderTens = expr.slice(i, i + 10);
+
+        let doubleLetterInNum = [];
+ 
+     
+        for (let i = 0; i < 10; i += 2) {
+            // пушим по 2 символа в массив
+            doubleLetterInNum.push(decoderTens.slice(i, i + 2));
+        }
+        transferInNum.push(doubleLetterInNum);
+    }
+    // console.log(transferInNum)
+
+    let transferInMorse = [];
+
+   
+    for (let elem of transferInNum) {
+        let morseLetter = '';
+   
+        elem.forEach(symbol => {
+            if (symbol in defaultSymbol) {
+                morseLetter += defaultSymbol[symbol];
+            } else {
+                morseLetter += '';
+            }
+        });
+        transferInMorse.push(morseLetter);
+    }
+
+    let transfer = '';
+
+    transferInMorse.forEach(symbol => {
+        
+        if (symbol in MORSE_TABLE) {
+      
+            transfer += MORSE_TABLE[symbol];
+        } else {
+          
+            transfer += ' ';
+        }
+    });
+
+    return transfer;
 }
 
 module.exports = {
